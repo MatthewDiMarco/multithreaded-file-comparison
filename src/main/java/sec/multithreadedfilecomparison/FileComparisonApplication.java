66dec7c2083bc74e851b514406fd6cffdbd7332d
Matthew Di Marco <matthew.d.dimarco@gmail.com>
2021-09-08 17:44:24 +0800

    Setup GUI

---
+++ src/main/java/sec/multithreadedfilecomparison/FileComparisonApplication.java
@@ -0,0 +1,98 @@
+package sec.multithreadedfilecomparison;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import sec.multithreadedfilecomparison.model.ComparisonResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileComparisonApplication extends Application {
+
+    private TableView<ComparisonResult> resultTable = new TableView<ComparisonResult>();
+    private ProgressBar progressBar = new ProgressBar();
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("File Comparison Application");
+        stage.setMinWidth(600);
+
+        // Create toolbar
+        Button compareBtn = new Button("Compare...");
+        Button stopBtn = new Button("Stop");
+        ToolBar toolBar = new ToolBar(compareBtn, stopBtn);
+
+        // Set up button event handlers
+        compareBtn.setOnAction(event -> crossCompare(stage));
+        stopBtn.setOnAction(event -> stopComparison());
+
+        // Initialise progressbar
+        progressBar.setProgress(0.0);
+
+        TableColumn<ComparisonResult,String> file1Col = new TableColumn<>("File 1");
+        TableColumn<ComparisonResult,String> file2Col = new TableColumn<>("File 2");
+        TableColumn<ComparisonResult,String> similarityCol = new TableColumn<>("Similarity");
+
+        // The following tells JavaFX how to extract information from a ComparisonResult
+        // object and put it into the three table columns.
+        file1Col.setCellValueFactory(
+                (cell) -> new SimpleStringProperty(cell.getValue().getFile1()) );
+
+        file2Col.setCellValueFactory(
+                (cell) -> new SimpleStringProperty(cell.getValue().getFile2()) );
+
+        similarityCol.setCellValueFactory(
+                (cell) -> new SimpleStringProperty(
+                        String.format("%.1f%%", cell.getValue().getSimilarity() * 100.0)) );
+
+        // Set and adjust table column widths.
+        file1Col.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.40));
+        file2Col.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.40));
+        similarityCol.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.20));
+
+        // Add the columns to the table.
+        resultTable.getColumns().add(file1Col);
+        resultTable.getColumns().add(file2Col);
+        resultTable.getColumns().add(similarityCol);
+
+        // Add the main parts of the UI to the window.
+        BorderPane mainBox = new BorderPane();
+        mainBox.setTop(toolBar);
+        mainBox.setCenter(resultTable);
+        mainBox.setBottom(progressBar);
+        Scene scene = new Scene(mainBox);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    private void crossCompare(Stage stage) {
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setInitialDirectory(new File("."));
+        dc.setTitle("Choose directory");
+        File directory = dc.showDialog(stage);
+
+        System.out.println("Comparing files within " + directory + "...");
+
+        // todo
+        // setup threads
+    }
+
+    private void stopComparison() {
+        System.out.println("Stopping comparison...");
+
+        // todo
+        // interrupt threads
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
